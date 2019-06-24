d6278e9867b045f9b70fa508f35024c1db038b8b
David R Tarvin Jr <david.tarvin@gmail.com>
2019-06-23 22:21:11 -0500

    commit

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -18,6 +18,7 @@
  */
 
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 
 var auth_controller = require('../controllers/authController');
@@ -27,7 +28,7 @@ router.post('/auth/register', auth_controller.user_register);
 
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 
 // POST request for signing users in
